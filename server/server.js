883020d3761d2901655670edd2cf17dba448b06d
AlaskanBeard <9093441+AlaskanBeard@users.noreply.github.com>
2021-11-26 03:51:21 -0700

    HTTPS attempt number 2

--- server/server.js
+++ server/server.js
@@ -3,15 +3,17 @@ import app from './express'
 import mongoose from 'mongoose'
 import fs from 'fs'
 import https from 'https'
-var privateKey = fs.readFileSync( '/etc/letsencrypt/live/testfrontiertech.com/privkey.pem')
-var certificate = fs.readFileSync( '/etc/letsencrypt/live/testfrontiertech.com/cert.pem')
-
+var privateKey 
+var certificate 
 // Connection URL
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
+
+//Old server start
+/*
 https.createServer ({
   key: privateKey,
   cert: certificate
@@ -21,3 +23,21 @@ https.createServer ({
   }
   console.info('Server started on port %s.', config.port)
 })
+*/
+
+// ssl cert info
+var options = {
+  key: fs.readFileSync( '/etc/letsencrypt/live/testfrontiertech.com/privkey.pem'),
+  cert: fs.readFileSync( '/etc/letsencrypt/live/testfrontiertech.com/cert.pem')
+};
+// https server
+https.createServer(options, function (req, res) {
+  res.end('secure!');
+}).listen(443);
+
+// Redirect from http port 80 to https
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(80);
