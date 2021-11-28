901410d6fa439fe12ec0ee58115a89641f9a6317
AlaskanBeard <9093441+AlaskanBeard@users.noreply.github.com>
2021-11-28 03:09:53 -0700

    Rolled back CssBaseline changes and changed pallete type to dark

--- client/theme.js
+++ client/theme.js
@@ -10,10 +10,7 @@ const theme = createTheme({
       light: '#5c67a3',
       main: '#3f4771',
       dark: '#2e355b',
-      contrastText: '#fff',
-      background: {
-        default: "#303030"
-      }
+      contrastText: '#fff'
     },
     secondary: {
       light: '#ff79b0',
@@ -23,7 +20,7 @@ const theme = createTheme({
     },
       openTitle: '#3f4771',
       protectedTitle: pink['400'],
-      type: 'light'
+      type: 'dark'
     }
   })
 
