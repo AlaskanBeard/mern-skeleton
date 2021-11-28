fe1d6745ae677fb041c0f0a078228d430708b95a
AlaskanBeard <9093441+AlaskanBeard@users.noreply.github.com>
2021-11-28 02:00:23 -0700

    Replaced all material-ui with mui

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MainRouter from './MainRouter'
 import {BrowserRouter} from 'react-router-dom'
-import { ThemeProvider } from '@material-ui/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme'
 import { hot } from 'react-hot-loader'
 
--- server/express.js
+++ server/express.js
@@ -28,11 +28,6 @@ const app = express()
 //comment out before building for production
 devBundle.compile(app)
 
-// https redirect
-app.enable('trust proxy')
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
-})
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
