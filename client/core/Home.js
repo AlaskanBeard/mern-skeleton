a7114b55c81be2ebf66bd6f73baedb863abab170
AlaskanBeard <9093441+AlaskanBeard@users.noreply.github.com>
2021-11-28 04:14:36 -0700

    Update homepage

--- client/core/Home.js
+++ client/core/Home.js
@@ -4,7 +4,7 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
-import unicornbikeImg from './../assets/images/unicornbike.jpg'
+import CssBaseline from '@mui/material/CssBaseline'
 
 const useStyles = makeStyles(theme => ({
   card: {
